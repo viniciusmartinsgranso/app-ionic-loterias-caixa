bfb4fbc6fdc86a3558b43244a216eb3363217cae
Vinícius Martins Granso <viniciusmartinsg@hotmail.com>
2026-06-04 20:13:59 -0300

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,20 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { baseUrlInterceptor } from "./modules/interceptors/base-url.interceptor";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideHttpClient(withInterceptors([baseUrlInterceptor])),
   ],
